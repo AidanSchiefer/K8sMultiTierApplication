b0df3b0ba55b14b0c6ba98ef08efcd2b7d2693f3
Aidan Schiefer <aidanschiefer@gmail.com>
2026-08-24 16:54:14 -0600

    Created the foundation for the new Python API layer

---
+++ k8s-3tier-lab/APILayer.py
@@ -0,0 +1,7 @@
+# This will be the new API layer for the project
+# Goals:
+# 1. Be able to communicate with the loacl Postgres image using FastAPI
+# 2. Once able to communicate and get data, export this file as a Docker image to be pulled down in the future
+
+# import the FastAPI package
+from fastapi import FastAPI
